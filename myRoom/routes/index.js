47d0d888af9b1aa97fde4a415872a27978763262
mengqiui <18324465803@163.com>
2018-05-28 14:55:47 +0800

    调试通mysql
    暂时先调试走通mysql

--- myRoom/routes/index.js
+++ myRoom/routes/index.js
@@ -1,6 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+// µ¼ÈëMySQLÄ£¿é
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'chatroom'
+});
+
+connection.connect();
+
+connection.query('SELECT * from userinfo', function (error, results, fields) {
+  if (error) throw error;
+  for(var i=0;i<results.length;i++){
+    console.log(results);
+    //console.log('The results is: '+results[i].uid+' '+results[i].username+' '+results[i].content);
+  }
+  //console.log(results)
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'express' });
